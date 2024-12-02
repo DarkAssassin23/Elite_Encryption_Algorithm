a747ce5fecba4156f7f416ea39b5c6cc1f2254d4
DarkAssassin23 <15916504+DarkAssassin23@users.noreply.github.com>
2024-12-02 10:51:28 -0500

    Added swift-testing dependency, updated local dependencies path

--- src/Swift/Package.swift
+++ src/Swift/Package.swift
@@ -27,7 +27,8 @@ let package = Package(
         .testTarget(
             name: "EEATests",
             dependencies: [
-                "EEAUtils"
+                "EEAUtils",
+                .product(name: "Testing", package: "swift-testing"),
             ]
         ),
     ]
@@ -43,14 +44,19 @@ var useLocalDependencies: Bool {
 var dependencies: [Package.Dependency] {
     if useLocalDependencies {
         return [
-            .package(path: "../swift-crypto")
+            .package(path: "./swift-crypto"),
+            .package(path: "./swift-testing"),
         ]
     } else {
         return [
             .package(
                 url: "https://github.com/apple/swift-crypto.git",
                 from: "3.5.0"
-            )
+            ),
+            .package(
+                url: "https://github.com/swiftlang/swift-testing.git",
+                from: "510.0.0"
+            ),
         ]
     }
 }
