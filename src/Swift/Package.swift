bc7ccacc561a7cc72da3b64b6e32d1ca05577c0f
DarkAssassin23 <15916504+DarkAssassin23@users.noreply.github.com>
2024-12-02 16:33:15 -0500

    Added XCTest back and a conditional on which tests to use based on if Swift >= 6.0

--- src/Swift/Package.swift
+++ src/Swift/Package.swift
@@ -2,38 +2,6 @@
 
 import PackageDescription
 
-let package = Package(
-    name: "eea",
-    platforms: [
-        .macOS(.v10_15)
-    ],
-    products: [
-        .executable(name: "EEA", targets: ["EEA"])
-    ],
-    dependencies: dependencies,
-    targets: [
-        .target(
-            name: "EEAUtils",
-            dependencies: [
-                .product(name: "Crypto", package: "swift-crypto")
-            ]
-        ),
-        .executableTarget(
-            name: "EEA",
-            dependencies: [
-                "EEAUtils"
-            ]
-        ),
-        .testTarget(
-            name: "EEATests",
-            dependencies: [
-                "EEAUtils",
-                .product(name: "Testing", package: "swift-testing"),
-            ]
-        ),
-    ]
-)
-
 func hasEnvironmentVariable(_ name: String) -> Bool {
     return ProcessInfo.processInfo.environment[name] != nil
 }
@@ -41,22 +9,105 @@ func hasEnvironmentVariable(_ name: String) -> Bool {
 var useLocalDependencies: Bool {
     hasEnvironmentVariable("SWIFTCLI_USE_LOCAL_DEPS")
 }
-var dependencies: [Package.Dependency] {
-    if useLocalDependencies {
-        return [
-            .package(path: "./Dependencies/swift-crypto"),
-            .package(path: "./Dependencies/swift-testing"),
-        ]
-    } else {
-        return [
-            .package(
-                url: "https://github.com/apple/swift-crypto.git",
-                from: "3.5.0"
+
+#if compiler(>=6.0)
+    let package = Package(
+        name: "eea",
+        platforms: [
+            .macOS(.v10_15)
+        ],
+        products: [
+            .executable(name: "EEA", targets: ["EEA"])
+        ],
+        dependencies: dependencies,
+        targets: [
+            .target(
+                name: "EEAUtils",
+                dependencies: [
+                    .product(name: "Crypto", package: "swift-crypto")
+                ]
             ),
-            .package(
-                url: "https://github.com/swiftlang/swift-testing.git",
-                from: "510.0.0"
+            .executableTarget(
+                name: "EEA",
+                dependencies: [
+                    "EEAUtils"
+                ]
+            ),
+            .testTarget(
+                name: "EEATests",
+                dependencies: [
+                    "EEAUtils",
+                    .product(name: "Testing", package: "swift-testing"),
+                ]
             ),
         ]
+    )
+
+    var dependencies: [Package.Dependency] {
+        if useLocalDependencies {
+            return [
+                .package(path: "./Dependencies/swift-crypto"),
+                .package(path: "./Dependencies/swift-testing"),
+            ]
+        } else {
+            return [
+                .package(
+                    url: "https://github.com/apple/swift-crypto.git",
+                    from: "3.5.0"
+                ),
+                .package(
+                    url: "https://github.com/swiftlang/swift-testing.git",
+                    from: "510.0.0"
+                ),
+            ]
+        }
     }
-}
+
+#else
+    let package = Package(
+        name: "eea",
+        platforms: [
+            .macOS(.v10_15)
+        ],
+        products: [
+            .executable(name: "EEA", targets: ["EEA"])
+        ],
+        dependencies: dependencies,
+        targets: [
+            .target(
+                name: "EEAUtils",
+                dependencies: [
+                    .product(name: "Crypto", package: "swift-crypto")
+                ]
+            ),
+            .executableTarget(
+                name: "EEA",
+                dependencies: [
+                    "EEAUtils"
+                ]
+            ),
+            .testTarget(
+                name: "EEATests",
+                dependencies: [
+                    "EEAUtils"
+                ]
+            ),
+        ]
+    )
+
+    var dependencies: [Package.Dependency] {
+        if useLocalDependencies {
+            return [
+                .package(path: "./Dependencies/swift-crypto")
+            ]
+        } else {
+            return [
+                .package(
+                    url: "https://github.com/apple/swift-crypto.git",
+                    from: "3.5.0"
+                )
+            ]
+        }
+    }
+
+#endif
--- src/Swift/Tests/EEATests/EEATests.swift
+++ src/Swift/Tests/EEATests/EEATests.swift
@@ -1,96 +1,193 @@
 import Crypto
 import EEAUtils
-import Testing
 
-/// Struct To handle testing various components of EEA
-struct EEASwiftTests {
-    /// Test case to make sure key generation works
-    @Test func keyGenTest() {
-        let keygen = Keygen()
-        let crypto = EEA()
-        var keys: [String]
-        var passed: Bool = true
-        do {
-            for x in (1...10) {
-                let size: Int = Int.random(in: 1...10) * 256
-                keys = try keygen.genKeys(size: size, num: x)
-                #expect(
-                    keys.count == x,
-                    "Key Generation Test Failed: Number of keys invalid"
-                )
-                for key in keys {
-                    let length = (size / 4)
+#if compiler(>=6.0)
+    import Testing
+#else
+    import XCTest
+#endif
+
+#if compiler(>=6.0)
+    /// Struct To handle testing various components of EEA
+    struct EEASwiftTests {
+        /// Test case to make sure key generation works
+        @Test func keyGenTest() {
+            let keygen = Keygen()
+            let crypto = EEA()
+            var keys: [String]
+            var passed: Bool = true
+            do {
+                for x in (1...10) {
+                    let size: Int = Int.random(in: 1...10) * 256
+                    keys = try keygen.genKeys(size: size, num: x)
                     #expect(
-                        key.count == length,
-                        "Key Generation Test Failed: Key length invalid"
+                        keys.count == x,
+                        "Key Generation Test Failed: Number of keys invalid"
                     )
+                    for key in keys {
+                        let length = (size / 4)
+                        #expect(
+                            key.count == length,
+                            "Key Generation Test Failed: Key length invalid"
+                        )
+                    }
+                    try crypto.keyCheck(keys)
                 }
-                try crypto.keyCheck(keys)
+            } catch (_) {
+                passed = false
             }
-        } catch (_) {
-            passed = false
+            #expect(passed, "Key generation test failed")
         }
-        #expect(passed, "Key generation test failed")
+
+        /// Test case to make sure encrypting and decrypting text works
+        @Test func stringEncryptTest() {
+            let keygen = Keygen()
+            let crypto = EEA()
+            var keys: [String]!
+            var passed: Bool = true
+            do {
+                keys = try keygen.genKeys()
+            } catch (_) {
+                passed = false
+            }
+            #expect(passed, "String Encrypt Test Error: Key generation")
+
+            let ogString: String =
+                "This is a test string.\nIt will be encrypted."
+            guard let crypt = crypto.encryptText(text: ogString, keys: keys)
+            else {
+                passed = false
+                #expect(passed, "String Encrypt Test Error: Encrypting text")
+                return
+            }
+            guard let decrypt = crypto.decryptText(text: crypt, keys: keys)
+            else {
+                passed = false
+                #expect(passed, "String Encrypt Test Error: Decrypting text")
+                return
+            }
+            passed = decrypt == ogString
+            #expect(passed, "String Encrypt Test Failed")
+        }
+
+        /// Test case to make sure encrypting and decrypting files works
+        @Test func fileEncryptTest() {
+            let keygen = Keygen()
+            let crypto = EEA()
+            let io = FileIO()
+            var keys: [String]!
+            var passed: Bool = true
+            do {
+                keys = try keygen.genKeys()
+            } catch (_) {
+                passed = false
+            }
+            #expect(passed, "File Encrypt Test Error: Key generation")
+
+            let filename = "output.txt"
+            let ogString: String =
+                "This is a test string.\nIt will be encrypted."
+            let data: [UInt8] = Array(ogString.utf8)
+            _ = try? io.writeFile(data, filename: filename)
+            _ = try? crypto.encryptFile(inFile: filename, keys: keys)
+            _ = try? crypto.decryptFile(
+                inFile: String("\(filename).eea"),
+                keys: keys
+            )
+            let tmp = try? io.readFile(filename)
+
+            // Cleanup
+            let deleted = io.deleteFile(filename: filename)
+            #expect(deleted, "File Encrypt Test Error: Deleting \(filename)")
+
+            passed = passed && (tmp == data)
+            #expect(passed, "File Encrypt Test Failed")
+        }
+    }
+#else
+    enum TestError: Error {
+        case nilValue(String)
     }
 
-    /// Test case to make sure encrypting and decrypting text works
-    @Test func stringEncryptTest() {
-        let keygen = Keygen()
-        let crypto = EEA()
-        var keys: [String]!
-        var passed: Bool = true
-        do {
-            keys = try keygen.genKeys()
-        } catch (_) {
-            passed = false
-        }
-        #expect(passed, "String Encrypt Test Error: Key generation")
+    /// Class To handle testing various components of EEA
+    class EEATests: XCTestCase {
+        private var crypto: EEA!
+        private var io: FileIO!
+        private var keygen: Keygen!
+        private var passed: Bool!
 
-        let ogString: String = "This is a test string.\nIt will be encrypted."
-        guard let crypt = crypto.encryptText(text: ogString, keys: keys) else {
-            passed = false
-            #expect(passed, "String Encrypt Test Error: Encrypting text")
-            return
+        /// Override the setUpWithError to run before each test
+        override func setUpWithError() throws {
+            try super.setUpWithError()
+            crypto = EEA()
+            io = FileIO()
+            keygen = Keygen()
+            passed = true
         }
-        guard let decrypt = crypto.decryptText(text: crypt, keys: keys) else {
-            passed = false
-            #expect(passed, "String Encrypt Test Error: Decrypting text")
-            return
+
+        /// Override the tearDownWithError to run after each test
+        override func tearDownWithError() throws {
+            try super.tearDownWithError()
+            crypto = nil
+            io = nil
+            keygen = nil
+            passed = true
         }
-        passed = decrypt == ogString
-        #expect(passed, "String Encrypt Test Failed")
+
+        /// Test case to make sure key generation works
+        func testKeygen() throws {
+            var keys: [String]
+            do {
+                keys = try keygen.genKeys()
+                try crypto.keyCheck(keys)
+            } catch (let e) {
+                throw e
+            }
+            XCTAssertTrue(passed)
+        }
+
+        /// Test case to make sure encrypting and decrypting text works
+        func testStringEncrypt() throws {
+            var keys: [String]
+            do {
+                keys = try keygen.genKeys()
+            } catch (let e) {
+                throw e
+            }
+
+            let ogString: String =
+                "This is a test string.\nIt will be encrypted."
+            guard let crypt = crypto.encryptText(text: ogString, keys: keys)
+            else {
+                throw TestError.nilValue("Encrypting text failed")
+            }
+            guard let decrypt = crypto.decryptText(text: crypt, keys: keys)
+            else {
+                throw TestError.nilValue("Decrypting text failed")
+            }
+            passed = decrypt == ogString
+            XCTAssertTrue(passed)
+        }
+
+        /// Test case to make sure encrypting and decrypting files works
+        func testFileEncrypt() throws {
+            var keys: [String]
+            do {
+                keys = try keygen.genKeys()
+            } catch (let e) {
+                throw e
+            }
+
+            let ogString: String =
+                "This is a test string.\nIt will be encrypted."
+
+            let data: [UInt8] = Array(ogString.utf8)
+            _ = try? io.writeFile(data, filename: "output.txt")
+            _ = try? crypto.encryptFile(inFile: "output.txt", keys: keys)
+            _ = try? crypto.decryptFile(inFile: "output.txt.eea", keys: keys)
+            let tmp = try? io.readFile("output.txt")
+            passed = passed && (tmp == data)
+            XCTAssertTrue(passed)
+        }
     }
-
-    /// Test case to make sure encrypting and decrypting files works
-    @Test func fileEncryptTest() {
-        let keygen = Keygen()
-        let crypto = EEA()
-        let io = FileIO()
-        var keys: [String]!
-        var passed: Bool = true
-        do {
-            keys = try keygen.genKeys()
-        } catch (_) {
-            passed = false
-        }
-        #expect(passed, "File Encrypt Test Error: Key generation")
-
-        let filename = "output.txt"
-        let ogString: String = "This is a test string.\nIt will be encrypted."
-        let data: [UInt8] = Array(ogString.utf8)
-        _ = try? io.writeFile(data, filename: filename)
-        _ = try? crypto.encryptFile(inFile: filename, keys: keys)
-        _ = try? crypto.decryptFile(
-            inFile: String("\(filename).eea"),
-            keys: keys
-        )
-        let tmp = try? io.readFile(filename)
-
-        // Cleanup
-        let deleted = io.deleteFile(filename: filename)
-        #expect(deleted, "File Encrypt Test Error: Deleting \(filename)")
-
-        passed = passed && (tmp == data)
-        #expect(passed, "File Encrypt Test Failed")
-    }
-}
+#endif
